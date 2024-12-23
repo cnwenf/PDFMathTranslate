4a2c4eb16f6895a7fbda3695e61002f15910518f
wenfeng.wf <wenfeng.wf@alibaba-inc.com>
2024-12-23 15:04:08 +0800

    add google adSense

--- pdf2zh/gui.py
+++ pdf2zh/gui.py
@@ -232,6 +232,9 @@ with gr.Blocks(
             el.dispatchEvent(new Event('input'));
         };
     </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8666811484806886"
+     crossorigin="anonymous">
+     </script>
     """
         if flag_demo
         else ""
